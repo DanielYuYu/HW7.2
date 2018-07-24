e2ac8de344c5e65d1abfe1a42f3c726fba820142
Daniel <daniel@macbook-pro.local>
2018-07-24 16:16:17 +0800

    first commit

--- HW7.2/ViewController.swift
+++ HW7.2/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var bubbleSwitch: UISwitch!
+    @IBOutlet weak var sweetLabel: UILabel!
+    @IBOutlet weak var currentImageView: UIImageView!
+    @IBAction func sweetSlider(_ sender: UISlider) {
+        sweetLabel.text = "\(String(format: "%0.f", sender.value))"
+    }
+    
+    @IBAction func commitButton(_ sender: Any) {
+        currentImageView.isHidden = false
+        if segmentControl.selectedSegmentIndex == 0 && bubbleSwitch.isOn {
+            currentImageView.image = UIImage(named: "ice1")
+        }
+        else if segmentControl.selectedSegmentIndex == 0 && !bubbleSwitch.isOn{
+            currentImageView.image = UIImage(named: "ice2")
+        }
+        else if segmentControl.selectedSegmentIndex == 1 && bubbleSwitch.isOn{
+            currentImageView.image = UIImage(named: "hot1")
+        }
+        else if segmentControl.selectedSegmentIndex == 1 && !bubbleSwitch.isOn{
+            currentImageView.image = UIImage(named: "hot2")
+        }
+        else{
+             currentImageView.image = UIImage(named: "tea")
+        }
+    }
 }
 
